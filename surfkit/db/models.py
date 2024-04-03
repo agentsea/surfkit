6564102334f3d33fa063e899215bb3ec49802373
Patrick Barker <patrickbarkerco@gmail.com>
2024-04-02 22:02:58 -0600

    clean

--- surfkit/db/models.py
+++ surfkit/db/models.py
@@ -4,71 +4,9 @@ import time
 from sqlalchemy import Column, String, ForeignKey, Boolean, Float, Integer
 from sqlalchemy.orm import relationship, declarative_base
 
-from guisurfer.server.models import V1UserProfile
+from ..models import V1UserProfile
 
 Base = declarative_base()
-
-
-class SSHKeyRecord(Base):
-    __tablename__ = "ssh_keys"
-
-    id = Column(String, primary_key=True, index=True)
-    owner_id = Column(String, nullable=False)
-    public_key = Column(String, unique=True, index=True)
-    private_key = Column(String)
-    name = Column(String, index=True)
-    created = Column(Float)
-    full_name = Column(String, unique=True, index=True)
-    metadata_ = Column(String)
-
-
-class SharedDesktopRuntimeRecord(Base):
-    __tablename__ = "shared_desktop_runtimes"
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    runtime_id = Column(String, ForeignKey("desktop_runtimes.id"), nullable=False)
-    shared_with_user_id = Column(String, nullable=False)
-    # Additional fields like share_date can be added here
-    runtime = relationship("DesktopRuntimeRecord", back_populates="shared_with")
-
-
-class DesktopRuntimeRecord(Base):
-    __tablename__ = "desktop_runtimes"
-
-    id = Column(String, primary_key=True, index=True)
-    owner_id = Column(String, nullable=False)
-    name = Column(String, index=True)
-    provider = Column(String, index=True)
-    credentials = Column(String)
-    created = Column(Float)
-    updated = Column(Float)
-    full_name = Column(String, unique=True, index=True)
-    metadata_ = Column(String)
-    shared_with = relationship("SharedDesktopRuntimeRecord", back_populates="runtime")
-
-
-class SharedAgentRuntimeRecord(Base):
-    __tablename__ = "shared_agent_runtimes"
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    runtime_id = Column(String, ForeignKey("agent_runtimes.id"), nullable=False)
-    shared_with_user_id = Column(String, nullable=False)
-    runtime = relationship("AgentRuntimeRecord", back_populates="shared_with")
-
-
-class AgentRuntimeRecord(Base):
-    __tablename__ = "agent_runtimes"
-
-    id = Column(String, primary_key=True, index=True)
-    owner_id = Column(String, nullable=False)
-    name = Column(String, index=True)
-    provider = Column(String, index=True)
-    credentials = Column(String)
-    created = Column(Float)
-    updated = Column(Float)
-    full_name = Column(String, unique=True, index=True)
-    metadata_ = Column(String)
-    shared_with = relationship("SharedAgentRuntimeRecord", back_populates="runtime")
 
 
 class UserRecord(Base):
@@ -89,26 +27,6 @@ class UserRecord(Base):
             created=self.created,
             updated=self.updated,
         )
-
-
-class AgentRecord(Base):
-    __tablename__ = "agents"
-
-    id = Column(String, primary_key=True)
-    name = Column(String, nullable=False)
-    owner_id = Column(String, nullable=False)
-    type = Column(String, nullable=False)
-    runtime = Column(String, nullable=False)
-    status = Column(String, nullable=False)
-    secrets = Column(String, nullable=True)
-    desktop = Column(String, nullable=True)
-    create_job_id = Column(String, nullable=True)
-    created = Column(Float, nullable=False)
-    updated = Column(Float, nullable=False)
-    metadata_ = Column(String, nullable=True)
-    envs = Column(String, nullable=True)
-    icon = Column(String, nullable=True)
-    full_name = Column(String, unique=True, index=True)
 
 
 class AgentTypeRecord(Base):
@@ -132,22 +50,3 @@ class AgentTypeRecord(Base):
     cpu_limit = Column(String)
     gpu_mem = Column(String)
     llm_providers = Column(String, nullable=True)
-
-
-class JobRecord(Base):
-    __tablename__ = "jobs"
-
-    id = Column(String, primary_key=True, default=lambda: str(uuid.uuid4()))
-    owner_id = Column(String, nullable=False)
-    name = Column(String, nullable=False)
-    type = Column(String, nullable=False)
-    status = Column(String, nullable=False)
-    runtime = Column(String, nullable=False)
-    namespace = Column(String, nullable=True)
-    phase = Column(String, nullable=True)
-    logs = Column(String, nullable=True)
-    result = Column(String, nullable=True)
-    created = Column(Float, default=time.time)
-    updated = Column(Float, default=time.time)
-    finished = Column(Float, default=0.0)
-    metadata_ = Column(String, nullable=True)
